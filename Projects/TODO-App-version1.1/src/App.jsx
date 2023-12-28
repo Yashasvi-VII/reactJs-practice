9e42aeb2c3711d4ae4107271c89bf4661e4a4c60
Yashasvi-VII <yashasvi9987@gmail.com>
2023-12-29 00:04:33 +0530

    Made all the app in single component ,used bootstrap for buttons and inputs

--- Projects/TODO-App-version1.1/src/App.jsx
+++ Projects/TODO-App-version1.1/src/App.jsx
@@ -4,9 +4,37 @@ function App() {
       <h1>TODO APP</h1>
       <div class="container text-center">
         <div class="row">
-          <div class="col-6">Col 4</div>
-          <div class="col-4">Col 4</div>
-          <div class="col-2">Col 2</div>
+          <div class="col-6">
+            <input type="text" placeholder="Enter TODO here " />
+          </div>
+          <div class="col-4">
+            <input type="date" />
+          </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-success">
+              Add
+            </button>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-6">Buy Milk</div>
+          <div class="col-4">4/10/23</div>
+          <div class="col-2">
+            <button type="button" class="btn btn-danger">
+              Delete
+            </button>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-6">Go to college</div>
+          <div class="col-4">5/11/23</div>
+          <div class="col-2">
+            <button type="button" class="btn btn-danger">
+              Delete
+            </button>
+          </div>
         </div>
       </div>
     </center>
