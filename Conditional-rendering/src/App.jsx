7c72d0273083cb1d5f8c40727119173191a59cc9
Yashasvi-VII <yashasvi9987@gmail.com>
2024-01-01 15:47:41 +0530

    Ternary and logical method of conditional rendering demo code

--- Conditional-rendering/src/App.jsx
+++ Conditional-rendering/src/App.jsx
@@ -3,17 +3,21 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  //"Dal", "Salad", "Green Vegetables", "Milk"
+  //let foodItems=["Dal", "Salad", "Green Vegetables", "Milk"];
   let foodItems = [];
 
   // First way of Conditional Rendering
-  if (foodItems.length == 0) {
-    return <h3>Resturant Out of Stock</h3>;
-  }
+  // if (foodItems.length == 0) {
+  //   return <h3>Resturant out of stock</h3>;
+  // }
 
   return (
     <>
-      <h1>Healthy Food</h1>
+      <h1>Healthy Food Restraunt</h1>
+      {/* Ternary operator method */}
+      {/* {foodItems.length == 0 ? <h3>Resturant out of stock</h3> : null} */}
+      {/* Logical operator method */}
+      {foodItems.length == 0 && <h3>Resturant out of stock</h3>}
       <ul className="list-group">
         {foodItems.map((item) => (
           <li key={item} className="list-group-item">
