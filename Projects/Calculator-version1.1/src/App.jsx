ae08cf0df966e3d6b31af47528a28ea6f009362c
Yashasvi-VII <yashasvi9987@gmail.com>
2024-01-02 14:21:39 +0530

    Calculator version 1.1 folder setup, basic css done

---
+++ Projects/Calculator-version1.1/src/App.jsx
@@ -0,0 +1,20 @@
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div className={styles.calculator}>
+      <input type="text" className={styles.display} />
+      <div className={styles.buttonsContainer}>
+        <button className={styles.button}>C</button>
+        <button className={styles.button}>1</button>
+        <button className={styles.button}>2</button>
+        <button className={styles.button}>3</button>
+        <button className={styles.button}>4</button>
+        <button className={styles.button}>5</button>
+        <button className={styles.button}>6</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
