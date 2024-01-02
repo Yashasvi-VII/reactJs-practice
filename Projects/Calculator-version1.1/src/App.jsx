54a8c8ea75abbb1ade363c64ea58ac1cd2d185ca
Yashasvi-VII <yashasvi9987@gmail.com>
2024-01-02 14:43:58 +0530

    made small components of calculator ,along with css modules,demo of displaying similar type data in array with help of map

--- Projects/Calculator-version1.1/src/App.jsx
+++ Projects/Calculator-version1.1/src/App.jsx
@@ -1,18 +1,12 @@
 import styles from "./App.module.css";
-
+import Display from "./components/Display";
+import ButtonsContainer from "./components/ButtonsContainer";
 function App() {
   return (
     <div className={styles.calculator}>
-      <input type="text" className={styles.display} />
-      <div className={styles.buttonsContainer}>
-        <button className={styles.button}>C</button>
-        <button className={styles.button}>1</button>
-        <button className={styles.button}>2</button>
-        <button className={styles.button}>3</button>
-        <button className={styles.button}>4</button>
-        <button className={styles.button}>5</button>
-        <button className={styles.button}>6</button>
-      </div>
+      {/* <input type="text" className={styles.display} /> */}
+      <Display />
+      <ButtonsContainer />
     </div>
   );
 }
