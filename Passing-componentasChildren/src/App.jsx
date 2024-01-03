19d31d3324325fa55de6f7ae6c5c135cc2c8d49c
Yashasvi-VII <yashasvi9987@gmail.com>
2024-01-03 12:46:18 +0530

    Passed children to generic container,Recived via props by destructuring it and added module css to container

--- Passing-componentasChildren/src/App.jsx
+++ Passing-componentasChildren/src/App.jsx
@@ -3,14 +3,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import FoodItems from "./components/FoodItems";
 import ErrorMessage from "./components/ErrorMessage";
+import Container from "./components/Container";
 function App() {
   let foodItems = ["Dal", "Salad", "Green Vegetables", "Milk"];
 
   return (
     <>
-      <h1 className="foodHeading">Healthy Food Restraunt</h1>
-      <ErrorMessage items={foodItems} />
-      <FoodItems items={foodItems} />
+      <Container>
+        <h1 className="foodHeading">Healthy Food Restraunt</h1>
+        <ErrorMessage items={foodItems} />
+        <FoodItems items={foodItems} />
+      </Container>
+      <Container>
+        <p>Above is list of healty food that are good for your well being</p>
+      </Container>
     </>
   );
 }
