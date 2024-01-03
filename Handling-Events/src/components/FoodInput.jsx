345b0c0231c638d06ae31d8e999c5d0697fe8479
Yashasvi-VII <yashasvi9987@gmail.com>
2024-01-03 14:17:37 +0530

    Added handel on change method to capture event object

--- Handling-Events/src/components/FoodInput.jsx
+++ Handling-Events/src/components/FoodInput.jsx
@@ -1,12 +1,16 @@
 import styles from "./FoodInput.module.css";
 
 const FoodInput = () => {
+  const handleOnChange = (event) => {
+    console.log(event.target.value);
+  };
   return (
     <input
       type="text"
       className={styles.foodInput}
       placeholder="Enter Food Item here"
-      onChange={(event) => console.log(event.target.value)}
+      onChange={(event) => handleOnChange(event)}
+      //  onChange={handleOnChange}; can be written in this way
     />
   );
 };
