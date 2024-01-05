a7055677d7a14b0699634020bfc6a046b6d16a8b
Yashasvi-VII <yashasvi9987@gmail.com>
2024-01-05 22:56:46 +0530

    useEffect hook folder setup previous code Social media app

---
+++ UseEffect-practice/src/store/post-list-store.jsx
@@ -0,0 +1,74 @@
+import { createContext, useReducer } from "react";
+
+const DEFAULT_CONTEXT = {
+  postList: [],
+  addPost: () => {},
+  addInitialPosts: () => {},
+  deletePost: () => {},
+};
+// Api/contract desiging
+export const PostList = createContext(DEFAULT_CONTEXT);
+
+const postListReducer = (currPostList, action) => {
+  let newPostList = currPostList;
+  if (action.type === "DELETE_POST") {
+    newPostList = currPostList.filter(
+      (post) => post.id !== action.payload.postId
+    );
+  } else if (action.type === "ADD_POST") {
+    newPostList = [action.payload, ...currPostList];
+  } else if (action.type === "ADD_INITIAL_POSTS") {
+    newPostList = action.payload.posts;
+  }
+  return newPostList;
+};
+
+const PostListProvider = ({ children }) => {
+  const [postList, dispatchPostList] = useReducer(postListReducer, []);
+
+  const addInitialPosts = (posts) => {
+    dispatchPostList({
+      type: "ADD_INITIAL_POSTS",
+      payload: {
+        posts: posts,
+      },
+    });
+  };
+
+  const addPost = (userId, postTitle, postBody, reactions, tags) => {
+    dispatchPostList({
+      type: "ADD_POST",
+      payload: {
+        id: Date.now(),
+        title: postTitle,
+        body: postBody,
+        reactions: reactions,
+        userId: userId,
+        tags: tags,
+      },
+    });
+  };
+  const deletePost = (postId) => {
+    dispatchPostList({
+      type: "DELETE_POST",
+      payload: {
+        postId,
+      },
+    });
+  };
+
+  return (
+    <PostList.Provider
+      value={{
+        postList: postList,
+        addPost: addPost,
+        addInitialPosts: addInitialPosts,
+        deletePost: deletePost,
+      }}
+    >
+      {children}
+    </PostList.Provider>
+  );
+};
+
+export default PostListProvider;
