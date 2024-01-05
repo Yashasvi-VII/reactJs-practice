d4b5d3441500f3437fe413d8bf7647185101df97
Yashasvi-VII <yashasvi9987@gmail.com>
2024-01-05 23:10:07 +0530

    useEffect code demo

--- UseEffect-practice/src/components/PostList.jsx
+++ UseEffect-practice/src/components/PostList.jsx
@@ -2,21 +2,24 @@ import { useContext } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
+import { useRef } from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
-  const handleGetPostsClick = () => {
+
+  useEffect(() => {
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then((data) => {
         addInitialPosts(data.posts);
       });
-  };
+  }, []);
+
   return (
     <>
-      {postList.length === 0 && (
-        <WelcomeMessage onGetPostsClick={handleGetPostsClick} />
-      )}
+      {postList.length === 0 && <WelcomeMessage />}
       {postList.map((post) => (
         <Post key={post.id} post={post} />
       ))}
--- UseEffect-practice/src/components/WelcomeMessage.jsx
+++ UseEffect-practice/src/components/WelcomeMessage.jsx
@@ -2,13 +2,6 @@ const WelcomeMessage = ({ onGetPostsClick }) => {
   return (
     <center className="welcome-message">
       <h1>There are no posts</h1>
-      <button
-        type="button"
-        className="btn btn-primary"
-        onClick={onGetPostsClick}
-      >
-        Get post from server
-      </button>
     </center>
   );
 };
