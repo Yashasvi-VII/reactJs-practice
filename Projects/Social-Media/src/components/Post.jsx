5e34a5a09e5357db21359fddc27fec8da8040bae
Yashasvi-VII <yashasvi9987@gmail.com>
2024-01-05 17:52:31 +0530

    Implemented delete

--- Projects/Social-Media/src/components/Post.jsx
+++ Projects/Social-Media/src/components/Post.jsx
@@ -1,5 +1,8 @@
+import { useContext } from "react";
 import { AiFillDelete } from "react-icons/ai";
+import { PostList } from "../store/post-list-store";
 const Post = ({ post }) => {
+  const { deletePost } = useContext(PostList);
   return (
     <div className="card post-card" style={{ width: "30rem" }}>
       <div className="card-body">
@@ -9,12 +12,18 @@ const Post = ({ post }) => {
             className="position-absolute top-0 start-100 
           translate-middle badge rounded-pill bg-danger"
           >
-            <AiFillDelete />
+            <AiFillDelete
+              onClick={() => {
+                deletePost(post.id);
+              }}
+            />
           </span>
         </h5>
         <p className="card-text">{post.body}</p>
         {post.tags.map((item) => (
-          <span className="badge text-bg-primary hastag">{item}</span>
+          <span key={item} className="badge text-bg-primary hastag">
+            {item}
+          </span>
         ))}
       </div>
       <div className="alert alert-success reactions">
