ea355bf56b675303d69d230eb37495c3a2da331d
Yashasvi-VII <yashasvi9987@gmail.com>
2024-01-05 11:26:21 +0530

    Made header footer sidebar component

--- Projects/Social-Media/src/App.jsx
+++ Projects/Social-Media/src/App.jsx
@@ -1,13 +1,21 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <>
-      <div>Placeholder</div>{" "}
-    </>
+    <div className="app-container">
+      <Sidebar />
+      <div className="content">
+        <Header />
+        <Footer />
+      </div>
+    </div>
   );
 }
 
