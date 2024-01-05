c5ba2c97ffbdac59278e1a6c427dc4396a2d868f
Yashasvi-VII <yashasvi9987@gmail.com>
2024-01-05 13:09:54 +0530

    made home and create post buttons fully functional using setSelectedTab

--- Projects/Social-Media/src/App.jsx
+++ Projects/Social-Media/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
 
   return (
     <div className="app-container">
-      <Sidebar selectedTab={selectedTab} />
+      <Sidebar selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
       <div className="content">
         <Header />
         {selectedTab === "Home" ? (
