0f43129ef3854b61091b997c6761e92e01817d30
Yashasvi-VII <yashasvi9987@gmail.com>
2024-01-05 11:38:53 +0530

    completed basic layout work

--- Projects/Social-Media/src/App.jsx
+++ Projects/Social-Media/src/App.jsx
@@ -6,6 +6,8 @@ import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Sidebar from "./components/Sidebar";
+import CreatePost from "./components/CreatePost";
+import PostList from "./components/PostList";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
       <Sidebar />
       <div className="content">
         <Header />
+        <CreatePost />
+        <PostList />
         <Footer />
       </div>
     </div>
--- Projects/Social-Media/src/components/Sidebar.jsx
+++ Projects/Social-Media/src/components/Sidebar.jsx
@@ -1,31 +1,31 @@
 const Sidebar = () => {
   return (
     <div
-      class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark sidebar"
+      className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark sidebar"
       style={{ width: "280px" }}
     >
       <a
         href="/"
-        class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none"
+        className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none"
       >
-        <svg class="bi pe-none me-2" width="40" height="32">
+        <svg className="bi pe-none me-2" width="40" height="32">
           <use xlinkHref="#bootstrap"></use>
         </svg>
-        <span class="fs-4">Sidebar</span>
+        <span className="fs-4">Sidebar</span>
       </a>
       <hr />
-      <ul class="nav nav-pills flex-column mb-auto">
-        <li class="nav-item">
-          <a href="#" class="nav-link active" aria-current="page">
-            <svg class="bi pe-none me-2" width="16" height="16">
+      <ul className="nav nav-pills flex-column mb-auto">
+        <li className="nav-item">
+          <a href="#" className="nav-link active" aria-current="page">
+            <svg className="bi pe-none me-2" width="16" height="16">
               <use xlinkHref="#home"></use>
             </svg>
             Home
           </a>
         </li>
         <li>
-          <a href="#" class="nav-link text-white">
-            <svg class="bi pe-none me-2" width="16" height="16">
+          <a href="#" className="nav-link text-white">
+            <svg className="bi pe-none me-2" width="16" height="16">
               <use xlinkHref="#speedometer2"></use>
             </svg>
             Create Post
