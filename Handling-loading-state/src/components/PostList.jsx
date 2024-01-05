33b78d1f03c61af3832a1c0f99cb6ba54650913b
Yashasvi-VII <yashasvi9987@gmail.com>
2024-01-05 23:45:04 +0530

    Handling loading state folder setup previous code social media app

---
+++ Handling-loading-state/src/components/PostList.jsx
@@ -0,0 +1,27 @@
+import Post from "./Post";
+import { PostList as PostListData } from "../store/post-list-store";
+import WelcomeMessage from "./WelcomeMessage";
+
+import { useEffect, useContext } from "react";
+
+const PostList = () => {
+  const { postList, addInitialPosts } = useContext(PostListData);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        addInitialPosts(data.posts);
+      });
+  }, []);
+
+  return (
+    <>
+      {postList.length === 0 && <WelcomeMessage />}
+      {postList.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+    </>
+  );
+};
+export default PostList;
