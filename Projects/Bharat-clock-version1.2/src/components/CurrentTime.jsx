e16699fa6ad47773029f5e24c933c07e2b732066
Yashasvi-VII <yashasvi9987@gmail.com>
2024-01-06 00:27:20 +0530

    Bharat clock version 1.2 completed

--- Projects/Bharat-clock-version1.2/src/components/CurrentTime.jsx
+++ Projects/Bharat-clock-version1.2/src/components/CurrentTime.jsx
@@ -1,5 +1,21 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 let CurrentTime = () => {
-  let time = new Date();
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    console.log("interval has been setup");
+
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+      console.log("canclled the interval");
+    };
+  }, []);
   return (
     <p>
       This is current Date and Time :- {time.toLocaleDateString()} -{" "}
